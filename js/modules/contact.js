30572b498da1eda47190fc39db0339ff09817fbe
jacksonvfo <jacksonvfo@gmail.com>
2020-08-18 16:37:13 -0300

    final commit

--- js/modules/contact.js
+++ js/modules/contact.js
@@ -6,26 +6,12 @@ export async function contact() {
     "<div id='contact'>\
       <img src='../../img/contact.jpg'/>\
       <div id='fluent-container'>\
-        <form id='contact-content' method='post'>\
+        <div id='contact-content'>\
           <div id='transparent-area'>\
             <h2>Entre em contato comigo</h2>\
-            <div id='not-message-wrapper' class='form-item'>\
-              <div>\
-              <input type='hidden' name='contact_number'>\
-              <label for='name'>nome</label>\
-              <input id='name-input' type='text' name='name' autocomplete='off' autofocus required>\
-              </div>\
-              <div>\
-              <label for='email'>email</label>\
-              <input id='email-input' type='email' name='email' required>\
-              </div>\
-            </div>\
-            <div id='message-wrapper' class='form-item'>\
-              <textarea  id='message-input'  name='message' rows='5' columns='50' autocomplete='off required'\
-              placeholder='\n\ndeixe sua mensagem aqui'></textarea>\
-            </div>\
-            <div id='submit-button' class='form-item'>\
-              <button type='submit' name='submit-button'>Enviar</button>\
+            <h4>Ficarei feliz em te responder :D</h4>\
+            <div id='contact-text'>\
+              <h5>PS: a não ser que você queira falar sobre esquemas de pirâmide</h5>\
             </div>\
           </div>\
           <div id='black-background'>\
@@ -43,46 +29,13 @@ export async function contact() {
               <p id='whats-p' class='hidden'> +55 45 99929-8962 </p>\
             </div>\
           </div>\
-        </form>\
+        </div>\
         <div id='transparent-background' class='noise'></div>\
       </div>\
     </div>";
 
   document.getElementById('apresentacao').innerHTML = htmlElement;
 
-  await sleep(100);
-
-  
-  document.getElementById('contact-content').addEventListener('submit', async function(event) {
-    event.preventDefault();
-
-    try{    
-      this.contact_number.value = Math.random() * 100000 | 0;
-      returned = await emailjs.sendForm('gmail', 'template_PkCjE1vS', this);
-        console.log(returned);
-      if (returned === 400){
-        throw(error);
-      }
-    } catch{
-      document.getElementById('email-confirmation').innerHTML = "<p>Houve algum problema</p>";
-      document.getElementById('email-confirmation').style.setProperty('background-color', '#FF5555');
-    }
-
-    
-    document.getElementById('name-input').value = '';
-    document.getElementById('email-input').value = '';
-    document.getElementById('message-input').value = '';
-
-    
-    document.getElementById('email-confirmation').classList.replace('hidden', 'moveIn');
-
-    await sleep(2500);
-
-    document.getElementById('email-confirmation').classList.replace('moveIn', 'moveOut');
-    await sleep(500);
-
-    document.getElementById('email-confirmation').classList.replace('moveOut', 'hidden');
-  });
   eventListenersMouseOver();
 };
 
