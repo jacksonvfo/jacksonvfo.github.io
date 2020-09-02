1098edfac593e79cf7c69e44b0fbb9ebbd52ef68
jacksonvfo <jacksonvfo@gmail.com>
2020-09-02 16:25:18 -0300

    1.1 about cards layout changed

--- js/modules/about.js
+++ js/modules/about.js
@@ -17,15 +17,78 @@ export async function about() {
 
   htmlElement +=
     "<div id='about-outer-flex2'>\
-        <div class='about-card-banner' id='about-card1'>\
+      <div id='about-outer-flex'>\
+        <div class='about-card-banner'>\
           <div id='img-wall-wrapper'>\
             <div id='img-wall'>\
-              <div class= 'vertical'></div>\
-              <img class='about-card-list' id='wall1' src='../../img/ds3.jpg'/>\
-              <img class='about-card-list' id='wall2' src='../../img/hardware.jpg'/>\
-              <img class='about-card-list' id='wall3' src='../../img/physics.jpg'/>\
+              <div >\
+                <div id='backdrop1' class='noise backdrop'></div>\
+                <div class='card-container' id='backdrop-origin1'>\
+                  <div class='about-card-header'>\
+                    <img class='about-card-list' id='wall1' src='../../img/ds3.jpg'/>\
+                    <div class='about-card-header-text'>\
+                      <h3> Games </h3>\
+                      <p> 4K : on </p>\
+                      <p> 60FPS : on </p>\
+                      <p> Ultra : on </p>\
+                    </div>\
+                  </div>\
+                  <div id='about-card1-text'>\
+                    <div id='about-text2' class='about-text'>\
+                      <p> Meu passatempo mais antigo. Uma boa jogabilidade, aliada a uma boa história,\
+                      fazem com que eu possa passar horas a fio imerso em um mundo a se explorar.\
+                      Meus gêneros favoritos são Action RPG, Corrida, Terror/horror e estratégia. </p>\
+                    </div>\
+                  </div>\
+                </div>\
+              </div>\
+              <div >\
+                <div  id='backdrop2' class='noise backdrop'></div>\
+                <div class='card-container' id='backdrop-origin2'>\
+                  <div class='about-card-header'>\
+                    <img class='about-card-list ' id='wall2' src='../../img/hardware.jpg'/>\
+                    <div class='about-card-header-text'>\
+                      <h3> Hardware </h3>\
+                      <p> Meu amigo, </p>\
+                      <p> isso ai vai gargalar </p>\
+                    </div>\
+                  </div>\
+                  <div id='about-card2-text'>\
+                    <div id='about-text3' class='about-text'>\
+                      <p> Como consequência dos games, veio o interesse por hardware de desktop.\
+                      Ja se passaram algumas noites em que fiquei estudando CPUs, GPUs e todo o\
+                      setup para encontrar a melhor combinação possível para as minhas necessidades.\
+                      Obviamente, descobrir porque a peça x é melhor que a y em combinação com a peça\
+                      z, é a parte mais divertida.</p>\
+                    </div>\
+                  </div>\
+                </div>\
+              </div>\
+              <div>\
+                <div id='backdrop3' class='noise backdrop'></div>\
+                <div class='card-container' id='backdrop-origin3'>\
+                  <div class='about-card-header'>\
+                    <img class='about-card-list' id='wall3' src='../../img/physics.jpg'/>\
+                    <div class='about-card-header-text'>\
+                      <h3> Ciência </h3>\
+                      <p> Cara, eu to falando, </p>\
+                      <p> tinha um gato vivo </p>\
+                      <p> nessa caixa </p>\
+                    </div>\
+                  </div>\
+                  <div id='about-card3-text'>\
+                    <div id='about-text4' class='about-text'>\
+                      <p> Nenhum hobby me ensinou tanto quanto o de saciar minha curiosidade\
+                      de como o mundo funciona. Seja em como\
+                      os fenômenos que vemos no dia a dia funcionam, ou até mesmo, como os eventos\
+                      mais extremos do universo ocorrem..</p>\
+                    </div>\
+                  </div>\
+                </div>\
+              </div>\
             </div>\
           </div>\
+        </div>\
       </div>\
     </div>";
 
@@ -33,98 +96,8 @@ export async function about() {
   document.getElementById('apresentacao').innerHTML = htmlElement;
   document.getElementById('apresentacao').style.setProperty('height', 'initial');
 
-  eventListenersMouseClick();
 }
 
 async function eventListenersMouseClick() {
-  let menu_about = document.querySelectorAll(".about-card-list");
-
-  menu_about.forEach(function (elem) {
-    elem.addEventListener("click", async function () {
-      let about_content;
-
-      if (elem.id === "wall1") {
-        about_content =
-          "<div id='about-outer-flex' class='outer-flex-bg-change1'>\
-            <div class='about-card-banner' id='about-card1'>\
-              <div id='img-wall-wrapper'>\
-                <div id='img-wall'>\
-                  <div class= 'vertical'></div>\
-                  <img class='about-card-list ' id='wall1' src='../../img/ds3.jpg'/>\
-                  <img class='about-card-list disabled' id='wall2' src='../../img/hardware.jpg'/>\
-                  <img class='about-card-list disabled' id='wall3' src='../../img/physics.jpg'/>\
-                </div>\
-              </div>\
-              <div id='about-text2' class='invisible-fast'>\
-                <h3> Games </h3>\
-                <p> Meu passatempo mais antigo. Uma boa jogabilidade, aliada a uma boa história,\
-                fazem com que eu possa passar horas a fio imerso em um mundo a se explorar.\
-                Meus gêneros favoritos são Action RPG, Corrida, Terror/horror e estratégia. </p>\
-              </div>\
-            </div>\
-          </div>"
-
-      }
-      else if (elem.id === "wall2") {
-        about_content =
-          "<div id='about-outer-flex'  class='outer-flex-bg-change2'>\
-            <div class='about-card-banner' id='about-card1'>\
-              <div id='img-wall-wrapper'>\
-                <div id='img-wall'>\
-                  <div class= 'vertical'></div>\
-                  <img class='about-card-list disabled' id='wall1' src='../../img/ds3.jpg'/>\
-                  <img class='about-card-list ' id='wall2' src='../../img/hardware.jpg'/>\
-                  <img class='about-card-list disabled' id='wall3' src='../../img/physics.jpg'/>\
-                </div>\
-              </div>\
-              <div id='about-text2' class='invisible-fast'>\
-                <h3> Hardware </h3>\
-                <p> Como consequência dos games, veio o interesse por hardware de desktop.\
-                Ja se passaram algumas noites em que fiquei estudando CPUs, GPUs e todo o\
-                setup para encontrar a melhor combinação possível para as minhas necessidades.\
-                Obviamente, descobrir porque a peça x é melhor que a y em combinação com a peça\
-                z, é a parte mais divertida.</p>\
-              </div>\
-            </div>\
-          </div>"
-      }
-      else if (elem.id === "wall3") {
-        about_content =
-          "<div id='about-outer-flex'  class='outer-flex-bg-change3'>\
-            <div class='about-card-banner' id='about-card1'>\
-              <div id='img-wall-wrapper'>\
-                <div id='img-wall'>\
-                  <div class= 'vertical'></div>\
-                  <img class='about-card-list disabled' id='wall1' src='../../img/ds3.jpg'/>\
-                  <img class='about-card-list disabled' id='wall2' src='../../img/hardware.jpg'/>\
-                  <img class='about-card-list ' id='wall3' src='../../img/physics.jpg'/>\
-                </div>\
-              </div>\
-              <div id='about-text2'  class='invisible-fast'>\
-                <h3> Ciência </h3>\
-                <p> Mas, nenhum passatempo me ensinou tanto quanto o de saciar minha curiosidade\
-                sobre como o mundo funciona. Seja na forma mais elementar da matéria, seja em como\
-                os fenômenos que vemos no dia a dia funcionam, ou até mesmo, como os eventos\
-                mais extremos do universo ocorrem. Dado isso, é claro que física é um dos\
-                meus assuntos preferidos.</p>\
-              </div>\
-            </div>\
-          </div>"
-      }
-
-      if(document.getElementById('about-outer-flex') === null){
-        document.getElementById('img-wall-wrapper').classList.add('reset-wall-position');
-        document.getElementById('about-outer-flex2').style.setProperty('transition', 'background-color 0.3s linear');
-        document.getElementById('about-outer-flex2').style.setProperty('background-color', '#07070A');
-
-        await sleep(700);
-      }
-
-      document.getElementById('about-outer-flex2').innerHTML = about_content;
-      await sleep(10);
-      document.getElementById('about-text2').classList.replace('invisible-fast', 'visible-10-fast');
-
-      eventListenersMouseClick();
-    });
-  });
+  
 }
